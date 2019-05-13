e3369f23a94b616c51451f523a62d2d029dfd8a6
Justin-Bee <jbee.appz@gmail.com>
2019-05-13 08:59:05 -0400

    pachy files

--- app/src/main/java/com/jbeeappz/dinodex/dinodex/MainActivity.java
+++ app/src/main/java/com/jbeeappz/dinodex/dinodex/MainActivity.java
@@ -147,39 +147,43 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 title = getString(R.string.mosasaurus);
                 break;
             case 15:
+                fragment = new pachycephalosaurus();
+                title = getString(R.string.pachycephalosaurus);
+                break;
+            case 16:
                 fragment = new pteranodon();
                 title = getString(R.string.pteranodon);
                 break;
 
-            case 16:
+            case 17:
                 fragment = new spinosaurus();
                 title = getString(R.string.spinosaurus);
                 break;
-            case 17:
+            case 18:
                 fragment = new stegosaurus();
                 title = getString(R.string.stegosaurus);
                 break;
-            case 18:
+            case 19:
                 fragment = new styracosaurus();
                 title = getString(R.string.styracosaurus);
                 break;
-            case 19:
+            case 20:
                 fragment = new therizonosaurus();
                 title = getString(R.string.therizonosaurus);
                 break;
-            case 20:
+            case 21:
                 fragment = new trex();
                 title = getString(R.string.trex);
                 break;
-            case 21:
+            case 22:
                 fragment = new triceratops();
                 title = getString(R.string.triceratops);
                 break;
-            case 22:
+            case 23:
                 fragment = new utahraptor();
                 title = getString(R.string.utahraptor);
                 break;
-            case 23:
+            case 24:
                 fragment = new velociraptor();
                 title = getString(R.string.velociraptor);
                 break;
