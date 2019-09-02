1ab4ab71ca8f786aba7d4696a64c13ee0aae9204
Justin-Bee <jbee.appz@gmail.com>
2019-09-02 14:04:22 -0400

    optimized for API 29, migrated to AndroidX

--- app/src/main/java/com/jbeeappz/dinodex/dinodex/NavigationDrawerAdaptor.java
+++ app/src/main/java/com/jbeeappz/dinodex/dinodex/NavigationDrawerAdaptor.java
@@ -2,7 +2,7 @@ package com.jbeeappz.dinodex.dinodex;
 
 import android.content.Context;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.view.LayoutInflater;
